dbf97ec61c1372ceb337318d22c31822d07aad87
ccasro <ccasro@gmail.com>
2025-10-15 12:44:36 +0200

    feat: add Maven module and initial project structure

---
+++ Java-Collections/src/main/java/org/ccasro/level1/exercise3/MainL1E3.java
@@ -0,0 +1,6 @@
+package org.ccasro.level1.exercise3;
+
+public class MainL1E3 {
+    public static void main(String[] args) {
+    }
+}
---
+++ Java-Collections/src/main/java/org/ccasro/level2/exercise2/MainL2E2.java
@@ -0,0 +1,6 @@
+package org.ccasro.level2.exercise2;
+
+public class MainL2E2 {
+    public static void main(String[] args) {
+    }
+}
